1fa8440a9ce1f7af26a414adcf840ccdcaa6ed16
Gabriel Henrique <gabrielhenrique9981@gmail.com>
2025-07-29 21:42:47 -0300

    alterado banco

--- db/db.py
+++ db/db.py
@@ -1,11 +1,19 @@
 # db.py
 import mysql.connector
 
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host='localhost',
+#         user='root',
+#         password='root',
+#         database='catalogo_psicologos'
+#     )
+
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        database='catalogo_psicologos'
+        host='sql113.infinityfree.com',
+        user='if0_39591777',
+        password='bK9WeRBZcl4j',
+        database='if0_39591777_XXX'
     )
