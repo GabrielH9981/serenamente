619b05e3a54c4fce5bb820fe0d02d0428fbbe3b9
Gabriel Henrique <gabrielhenrique9981@gmail.com>
2025-07-30 01:35:54 -0300

    Adicionado painel de administrador

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,8 @@ from .main import main_bp
 from .perfil import perfil_bp
 from .psicologos import psicologos_bp
 from .ativacao import ativacao_bp
+from .admin import admin_bp
+
 
 def register_routes(app):
     app.register_blueprint(auth_bp)
@@ -10,3 +12,4 @@ def register_routes(app):
     app.register_blueprint(perfil_bp)
     app.register_blueprint(psicologos_bp)
     app.register_blueprint(ativacao_bp)
+    app.register_blueprint(admin_bp)
